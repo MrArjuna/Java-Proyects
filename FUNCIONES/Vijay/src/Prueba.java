8ca28e40bf4aabf563ccde7a11b3b211a9b14e20
MrArjuna <128251686+MrArjuna@users.noreply.github.com>
2025-01-10 16:59:45 -0600

    Add files via upload

---
+++ FUNCIONES/Vijay/src/Prueba.java
@@ -0,0 +1,59 @@
+public class Prueba {
+
+    public void Saludo(){
+    System.out.println("Qiubo");
+    }
+
+    public void Saludo2 (String texto) {
+    System.out.println("Hola" + texto);
+
+    }
+
+    public String Salu4 (){
+    return "Que tranza Banda";
+
+    }
+
+
+    public String Salu5 (String Extra){
+    return "Hola123556";
+
+    }
+
+    public class Operacion {
+
+    public static int Multiplicar (int c, int d, int e){
+
+    return c*d*e;
+
+    }
+
+    }
+
+
+    public static void main(String[] args) {
+    
+    Prueba Vijay = new Prueba();
+    Prueba Vijay2 = new Prueba();
+
+    Vijay.Saludo();
+    Vijay2.Saludo2("World");
+
+    String Salu4I = Vijay.Salu4();
+
+    System.out.println(Salu4I);
+
+
+    String Extra1 = Vijay2.Salu5("123");
+
+    System.out.println(Extra1);
+
+    int resultadoM = Operacion.Multiplicar (2,3,5);
+
+    System.out.println(resultadoM);
+
+
+    }
+
+}
+    
